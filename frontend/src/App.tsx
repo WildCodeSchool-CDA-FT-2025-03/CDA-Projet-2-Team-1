369a6174c4b3b0c4be6a4e31088df879335ab81f
rodolphe <rodolphe.tournier@gmail.com>
2025-04-20 17:21:32 +0200

    feat: New Configuration for the frontend and backend services

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="card">
+        <p>
+          Bienvenue sur Care Plan
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
