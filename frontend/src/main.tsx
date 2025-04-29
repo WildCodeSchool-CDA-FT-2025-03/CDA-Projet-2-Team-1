4f14285f00a63fee64e2acd20049f7c5a9c797b1
rodolphe <rodolphe.tournier@gmail.com>
2025-04-29 18:22:02 +0200

    fix: rollback error merg 4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,11 @@
-import './index.css';
+import './index.css'
 
-import App from './App.tsx';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import App from './App.tsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-const root = createRoot(document.getElementById('root')!);
-root.render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </StrictMode>,
+)
