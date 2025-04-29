0769c6c80f11d93737be133dfa78781faa165411
Florian Sebal <fsebal91@gmail.com>
2025-04-29 11:43:45 +0200

    feat:création des pages secretaire docteur agent et admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css';
-
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
     <>
       <div className="card">
         <p>Bienvenue sur Care-Plan votre site de santé</p>
+        <Outlet />
       </div>
     </>
   );
