e96ffee352932ce52ee9dd6c1d8c62eb3c592f38
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-04-30 16:49:11 +0200

    Création du controller register

--- backend/email-service/src/router/router.ts
+++ backend/email-service/src/router/router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 
-// Import des sub route indépendante !
-
+// Import des controllers
+import registerController from "../controllers/registerController";
 
 const router = express.Router();
 
 /* Liste des routes ! */
+router.post("/register", registerController); // Route pour l'inscription d'un utilisateur
 
 export default router;
