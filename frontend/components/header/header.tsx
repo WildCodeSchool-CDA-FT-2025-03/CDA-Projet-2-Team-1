f828fa2e67f37636d28fe4f1145b76a29c1aa6a9
Florian Sebal <fsebal91@gmail.com>
2025-05-01 15:58:36 +0200

    feat:impléentation logo + import svg dans env-vite

--- frontend/components/header/header.tsx
+++ frontend/components/header/header.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import { HelpCircle } from 'lucide-react';
+import logocp from './logocp.svg';
 
 export default function Header() {
   return (
     <header className="w-full bg-white shadow px-6 py-4 flex items-center justify-between">
       {/* Logo + Titre */}
       <div className="flex flex-col">
-        <span className="text-2xl font-bold text-blue-600">CarePlan</span>
-        <span className="text-sm text-gray-500">
-          votre gestionnaire numérique
-        </span>
+        <img src={logocp} alt="logo" className="h-10 w-auto" />
       </div>
 
       {/* Boutons (non fonctionnels pour l'instant) */}
@@ -16,8 +15,8 @@ export default function Header() {
         <button className="px-4 py-2 border border-sky-600 rounded hover:bg-sky-600 text-sm">
           Déconnexion
         </button>
-        <button className="px-4 py-2 border border-sky-600 text-black rounded hover:bg-sky-600 text-sm">
-          Aide
+        <button className="px-4 py-2 text-sky rounded ">
+          <HelpCircle className="w-10 h-10 text-sky-600" />
         </button>
       </div>
     </header>
