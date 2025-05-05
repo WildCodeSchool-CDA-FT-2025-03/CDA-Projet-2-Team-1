718b6857eecec7283af81632a4b40828204db089
Yiroma <yiromaric@gmail.com>
2025-05-05 15:30:52 +0200

    fix: nettoyage des packages racine, update de eslintrc et lint-staged racine, nettoyage package.lock racine

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,21 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: [
+    'dist/**',
+    'node_modules/**',
+    'build/**',
+    'coverage/**',
+    '**/*.min.js',
+    '**/*.d.ts',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
   rules: {
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-non-null-assertion': 'off',
@@ -21,14 +36,27 @@ module.exports = {
         'plugin:react-hooks/recommended',
         'plugin:jsx-a11y/recommended',
       ],
+      plugins: ['react-refresh'],
       settings: {
         react: {
           version: 'detect',
         },
       },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       rules: {
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/jsx-no-constructed-context-values': 'warn',
+        'react/no-array-index-key': 'warn',
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       },
     },
     {
@@ -38,6 +66,14 @@ module.exports = {
         node: {
           tryExtensions: ['.js', '.json', '.node', '.ts', '.d.ts'],
         },
+      },
+    },
+    {
+      files: ['**/*.js', '**/*.cjs', '**/*.mjs', '**/vite.config.ts'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
   ],
