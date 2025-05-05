008de525ff15f670ff51ab9d08080b03790adfe4
Maximilien <maximilien.philippe@protonmail.com>
2025-05-05 16:46:04 +0200

    feat: index start apollo server witch resolver

--- backend/appointment-service/src/index.ts
+++ backend/appointment-service/src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello world');
+import { ApolloServer } from '@apollo/server';
+import { dataSource } from './services/client';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { buildSchema } from 'type-graphql';
+import PatientResolver from './resolvers/patient.resolver';
+
+(async () => {
+  await dataSource.initialize();
+
+  const schema = await buildSchema({
+    resolvers: [PatientResolver],
+  });
+
+  const server = new ApolloServer({
+    schema,
+  });
+
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`Server ready at: ${url}`);
+})();
