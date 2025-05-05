41114e4d6ad27813417cfd32b96e49d796801067
Yiroma <119810671+Yiroma@users.noreply.github.com>
2025-05-05 17:03:02 +0200

    Revert "Pcp 73 login page to router"

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -14,11 +14,7 @@ export default function Login() {
   return (
     <div className="min-h-screen flex flex-row w-full">
       <div className="hidden md:flex w-1/2 items-center justify-center bg-white">
-        <img
-          src="/imagelogin.svg"
-          alt="Illustration connexion"
-          className="max-w-full h-auto"
-        />
+        <img src="/imagelogin.svg" alt="Illustration connexion" className="max-w-full h-auto" />
       </div>
       <div className="w-full md:w-1/2 flex items-center justify-center bg-[#f9fbfd]">
         <div className="login-form-container">
@@ -34,9 +30,7 @@ export default function Login() {
                 placeholder="youremail@exemple.com"
                 required
                 value={formData.email}
-                onChange={(e) =>
-                  setFormData({ ...formData, email: e.target.value })
-                }
+                onChange={e => setFormData({ ...formData, email: e.target.value })}
                 className="login-input"
               />
             </div>
@@ -50,9 +44,7 @@ export default function Login() {
                 placeholder="yourpassword"
                 required
                 value={formData.password}
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
+                onChange={e => setFormData({ ...formData, password: e.target.value })}
                 className="login-input"
               />
             </div>
