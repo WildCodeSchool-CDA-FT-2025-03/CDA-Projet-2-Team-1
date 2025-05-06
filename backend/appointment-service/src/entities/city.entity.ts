9a5d308b689a4e632e4304bc57df9f2201b0ac79
Florian Sebal <fsebal91@gmail.com>
2025-05-06 16:23:16 +0200

    feat:entity city

---
+++ backend/appointment-service/src/entities/city.entity.ts
@@ -0,0 +1,20 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity('city')
+class CityEntity extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Field()
+  @Column({type:'varchar', nullable:false, length: 64})
+  name: string;
+
+  @Field()
+  @Column({type:'varchar', nullable:false, length: 16})
+  zip_code: string;
+}
+
+export default CityEntity;
