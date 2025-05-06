29e6534619a3eb98c2c2f064db2b00ce481b6d1f
Florian Sebal <fsebal91@gmail.com>
2025-05-06 14:33:55 +0200

    feat: entity ssn

--- backend/appointment-service/src/services/client.service.ts
+++ backend/appointment-service/src/services/client.service.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import PatientEntity from '../entities/patient.entity';
-
+import SsnEntity from '../entities/ssn.entity';
 const port = process.env.DATABASE_PORT ? +process.env.DATABASE_PORT : 5432;
 const sync = process.env.DATABASE_SYNC === 'true' ? true : false;
 
@@ -11,6 +11,6 @@ export const dataSource = new DataSource({
   username: process.env.DATABASE_USER || '',
   password: process.env.DATABASE_PASSWORD || '',
   port: port,
-  entities: [PatientEntity],
+  entities: [PatientEntity, SsnEntity],
   synchronize: sync,
 });
