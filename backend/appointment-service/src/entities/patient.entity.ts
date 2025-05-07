5d3eb5998c7110277b5b537267dee3c23bc3011a
Florian Sebal <fsebal91@gmail.com>
2025-05-07 13:55:46 +0200

    feat:patient entity update

--- backend/appointment-service/src/entities/patient.entity.ts
+++ backend/appointment-service/src/entities/patient.entity.ts
@@ -1,5 +1,5 @@
 import { Field, ObjectType } from 'type-graphql';
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, Timestamp } from 'typeorm';
 
 @ObjectType()
 @Entity('patient')
@@ -7,6 +7,22 @@ class PatientEntity extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Field()
+  @Column({type: 'varchar', nullable: false, length: 64})
+  firstname: string;
+
+  @Field()
+  @Column({type: 'varchar', nullable: false, length: 64})
+  lastname: string;
+
+  @Field()
+  @Column({type: 'timestamp', nullable: false, length: 64})
+  birthdate: Timestamp;
+
+  @Field()
+  @Column({type: 'number', nullable: false, length: 1})
+  gender: number;
 }
 
 export default PatientEntity;
