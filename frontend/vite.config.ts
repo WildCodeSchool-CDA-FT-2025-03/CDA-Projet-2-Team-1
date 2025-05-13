1de7619534306866c476c4ccfbf683b337f9a0d2
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-13 21:50:57 +0200

    Config de vite avec gestion des mode production et development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,54 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  root: './',
-  publicDir: './public',
-  server: {
-    host: process.env.VITE_HOST || '', // node container in docker (container name)
-    origin: 'http://localhost:5173', // exposed node container address
-  },
+
+export default defineConfig(() => {
+
+    // Récupère le chemin absolue ou est exécuté ce fichier
+    const root = process.cwd();
+
+    // On récupère la variable d'environnement injectée par Docker pour savoir si on est en prod ou dev
+    const viteNodeEnv = process.env.VITE_NODE_ENV || 'development';
+
+    let envBase = {};
+    let envRoot = {};
+
+    // Initialisation conditionnelle
+    if (viteNodeEnv === 'production') {
+        envBase = loadEnv('base', root, 'VITE_'); // .env.base
+        envRoot = loadEnv('production', root, 'VITE_'); // .env.root
+    } 
+    else if (viteNodeEnv === 'development') {
+        envBase = loadEnv('base', root, 'VITE_'); // peut être partagé
+        envRoot = loadEnv('development', root, 'VITE_'); // .env.development
+    }
+    
+
+    // Fusion manuelle des variables (ordre de priorité : root > base)
+    const env = {
+        ...envBase,
+        ...envRoot
+    };
+
+    return {
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+              '@': path.resolve(__dirname, './src'),
+            },
+        },
+        root: './',
+        publicDir: './public',
+        server: {
+            host: true,
+            port: parseInt(process.env.VITE_FRONTEND_PORT || '5173'), // Port exposé par le conteneur
+        },
+
+    // Redéfinit import.meta.env à la main pour bloquer tout chargement implicite, ne charge plus les .env par défauts
+    define: {
+        'import.meta.env': JSON.stringify(env)
+    }
+  };
 });
