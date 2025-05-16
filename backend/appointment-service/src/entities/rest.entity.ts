2632e7ff24c2954bd26ecc83a13ecb6c6db211b3
Maximilien <maximilien.philippe@protonmail.com>
2025-05-16 15:09:25 +0200

    feat: remove unused imports from rest.entity

--- backend/appointment-service/src/entities/rest.entity.ts
+++ backend/appointment-service/src/entities/rest.entity.ts
@@ -1,13 +1,5 @@
 import { Field, ObjectType } from 'type-graphql';
-import {
-  BaseEntity,
-  Column,
-  Entity,
-  JoinColumn,
-  JoinTable,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from 'typeorm';
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import UserEntity from './user.entity';
 
 @ObjectType()
