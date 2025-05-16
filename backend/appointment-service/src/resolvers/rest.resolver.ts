4ecafee6394cba47b4b7a4e41236b781be3eff0d
Maximilien <maximilien.philippe@protonmail.com>
2025-05-16 14:11:41 +0200

    feat: update getByUserID to include user relations

--- backend/appointment-service/src/resolvers/rest.resolver.ts
+++ backend/appointment-service/src/resolvers/rest.resolver.ts
@@ -6,7 +6,10 @@ import RestEntity from '../entities/rest.entity';
 class RestResolver {
   @Query(() => [RestEntity])
   async getByUserID(@Arg('userId', () => String) userId: string): Promise<RestEntity[]> {
-    return RestEntity.findBy({ user_id: userId });
+    return RestEntity.find({
+      where: { user: { id: userId } },
+      relations: ['user'],
+    });
   }
 }
 
