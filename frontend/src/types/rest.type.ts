a33a5587acd440423c0bee910801e4c3d2eb9203
Yiroma <yiromaric@gmail.com>
2025-05-16 16:44:30 +0200

    feature: calendrier rest dynamique avec l'id du médecin

--- frontend/src/types/rest.type.ts
+++ frontend/src/types/rest.type.ts
@@ -1,6 +1,11 @@
 export type RestEvent = {
   id: number;
+  user_id: string;
   start: Date;
   end: Date;
   type: 'Congé' | 'Maladie' | 'Formation';
+};
+
+export type RestProps = {
+  user_id: string;
 };
