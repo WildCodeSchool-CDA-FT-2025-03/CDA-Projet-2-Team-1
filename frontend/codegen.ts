8e1ed6ecc3a5abc13f103eb851b09d522c3edb18
Maximilien <maximilien.philippe@protonmail.com>
2025-05-20 12:17:00 +0200

    rework: rework codegen config and remove unused files

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -1,14 +1,20 @@
+import { defineConfig, loadEnv } from 'vite';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
+const root = process.cwd();
+
 const config: CodegenConfig = {
-  schema: 'http://localhost:4000/graphql',
-  documents: ['src/**/*.tsx', 'src/**/*.ts'],
+  schema: `${process.env.VITE_APOLLO_SERVER}/`,
+  documents: ['src/schemas/**/*.ts'],
   generates: {
-    './src/gql/': {
-      preset: 'client',
-      plugins: [],
+    './src/gql/graphql-types.ts': {
+      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
+      config: {
+        withHooks: true,
+      },
     },
   },
+  overwrite: true,
 };
 
 export default config;
