1c6fa5db6b65907001ac242cf56913a7caf46f10
Maximilien <maximilien.philippe@protonmail.com>
2025-05-20 16:01:23 +0200

    feat: add todo

--- backend/appointment-service/src/resolvers/rest.resolver.ts
+++ backend/appointment-service/src/resolvers/rest.resolver.ts
@@ -5,7 +5,9 @@ import RestEntity from '../entities/rest.entity';
 @Resolver(RestEntity)
 class RestResolver {
   @Query(() => [RestEntity])
+  // TODO: Auth
   async getByUserID(@Arg('userId', () => String) userId: string): Promise<RestEntity[]> {
+    // TODO: id from cooki
     return RestEntity.find({
       where: { user: { id: userId } },
       relations: ['user'],
