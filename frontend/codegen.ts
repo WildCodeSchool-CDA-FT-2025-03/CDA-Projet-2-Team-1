1b435d2f24639dddd27ae37717593b45e3e7956d
Yiroma <yiromaric@gmail.com>
2025-05-21 19:57:34 +0200

    ajout option si variable non reconnu dans la config codegen

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -2,7 +2,7 @@ import 'vite';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: `${process.env.VITE_APOLLO_SERVER}/`,
+  schema: process.env.VITE_APOLLO_SERVER || 'http://localhost:4000',
   documents: ['src/schemas/**/*.ts'],
   generates: {
     './src/gql/graphql-types.ts': {
