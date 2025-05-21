bb78c3be749de741c73b2a9b3c445dea9eea9569
Maximilien <maximilien.philippe@protonmail.com>
2025-05-21 17:47:32 +0200

    feat: add service entity and relation to user
    The new ServiceEntity includes basic fields and establishes a one-to-many
    relationship with UserEntity. UserEntity now has a required many-to-one
    relationship back to ServiceEntity with a foreign key column.

---
+++ backend/appointment-service/src/entities/service.entity.ts
@@ -0,0 +1,20 @@
+import { Field, ObjectType } from 'type-graphql';
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import UserEntity from './user.entity';
+
+@ObjectType()
+@Entity('service')
+class ServiceEntity extends BaseEntity {
+  @Field()
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Field()
+  @Column({ type: 'varchar', nullable: false, length: 64 })
+  name: string;
+
+  @OneToMany(() => UserEntity, (user) => user.service)
+  user: UserEntity[];
+}
+
+export default ServiceEntity;
--- backend/appointment-service/src/entities/user.entity.ts
+++ backend/appointment-service/src/entities/user.entity.ts
@@ -1,7 +1,15 @@
 import { Field, ObjectType } from 'type-graphql';
-import { BaseEntity, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 import RestEntity from './rest.entity';
 import ConsultationEntity from './consultation.entity';
+import ServiceEntity from './service.entity';
 
 @ObjectType()
 @Entity('user')
@@ -15,6 +23,11 @@ class UserEntity extends BaseEntity {
 
   @OneToMany(() => ConsultationEntity, (consultation) => consultation.doctor)
   consultation: ConsultationEntity[];
+
+  @Field(() => ServiceEntity, { nullable: false })
+  @ManyToOne(() => ServiceEntity, (service) => service.user, { nullable: false })
+  @JoinColumn({ name: 'service_id' })
+  service: ServiceEntity;
 }
 
 export default UserEntity;
