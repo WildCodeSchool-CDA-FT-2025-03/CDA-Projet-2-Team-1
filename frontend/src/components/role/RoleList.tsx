0567bb0a1611dc83f350703bdaa162e2b3be2216
Florian Sebal <fsebal91@gmail.com>
2025-05-22 16:08:40 +0200

    feat:rolelist avec UseState

--- frontend/src/components/role/RoleList.tsx
+++ frontend/src/components/role/RoleList.tsx
@@ -1,8 +1,16 @@
 import { useGetRolesQuery } from '@/gql/graphql-types';
+import { useState, useCallback } from 'react';
 
 export default function Rolelist() {
   const { data } = useGetRolesQuery();
   const roles = data?.getRoles || [];
+  const [selectedRole, setSelectedRole] = useState('admin');
+
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+    event.preventDefault();
+    setSelectedRole(event.target.value);
+  }, []);
+
   return (
     <div className="w-64">
       <label htmlFor="options" className="block mb-2 text-sm font-medium text-gray-700">
@@ -10,11 +18,14 @@ export default function Rolelist() {
       </label>
       <select
         id="options"
+        value={selectedRole}
+        onChange={handleChange}
         className="block w-full p-2.5 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm text-gray-700"
       >
-        <option>roles</option>
         {roles.map((role) => (
-          <option key={role.id}>{role.name}</option>
+          <option key={role.id} value={role.name}>
+            {role.name}
+          </option>
         ))}
       </select>
     </div>
