cf1d368079fa861d1f99b2c633fe921066d0165f
Florian Sebal <fsebal91@gmail.com>
2025-05-22 12:06:55 +0200

    feat: liaison back front rolelist

--- frontend/src/components/role/RoleList.tsx
+++ frontend/src/components/role/RoleList.tsx
@@ -1,4 +1,8 @@
+import { useGetRolesQuery } from '@/gql/graphql-types';
+
 export default function Rolelist() {
+  const { data } = useGetRolesQuery();
+  const roles = data?.getRoles || [];
   return (
     <div className="w-64">
       <label htmlFor="options" className="block mb-2 text-sm font-medium text-gray-700">
@@ -8,10 +12,10 @@ export default function Rolelist() {
         id="options"
         className="block w-full p-2.5 border border-gray-300 rounded-lg shadow-sm focus:ring-blue-500 focus:border-blue-500 text-sm text-gray-700"
       >
-        <option>Admin</option>
-        <option>Medecin</option>
-        <option>Secretaire</option>
-        <option>Agent</option>
+        <option>roles</option>
+        {roles.map((role) => (
+          <option key={role.id}>{role.name}</option>
+        ))}
       </select>
     </div>
   );
