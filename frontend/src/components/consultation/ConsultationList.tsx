7c0c444e73bcd0af042ae2b702322f5bc53b1284
Yiroma <yiromaric@gmail.com>
2025-05-23 14:06:51 +0200

    test du overflow sur une table

--- frontend/src/components/consultation/ConsultationList.tsx
+++ frontend/src/components/consultation/ConsultationList.tsx
@@ -1,7 +1,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -19,30 +18,79 @@ export default function ConsultationList({ date }: ConsultationProps) {
   // const res = req.data?.getConsultationByDay || [];
 
   return (
-    <section className="border-1 rounded-[6px] border-turquoise-600 w-1/2 max-h-[150px]">
-      <Table className="table-auto overflow-scroll w-full">
-        <TableCaption className="caption-top text-left text-2xl">
-          Liste des patients par horaires
-        </TableCaption>
-        <TableHeader className="">
-          <TableRow className="border-gray-300">
-            <TableHead className="w-[100px] font-bold">Heure</TableHead>
-            <TableHead className="text-right font-bold">Nom</TableHead>
-            <TableHead className="text-right font-bold">Prénom</TableHead>
-            <TableHead className="text-right font-bold">Médecin</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody className="overflow-hidden">
-          {req.data?.getConsultationByDay.map((el) => (
-            <TableRow className="border-none" key={el.id}>
-              <TableCell>{dateToTime(new Date(el.date_start))}</TableCell>
-              <TableCell className="text-right">{el.patient.firstName}</TableCell>
-              <TableCell className="text-right">{el.patient.lastName}</TableCell>
-              <TableCell className="text-right font-bold">Dr. {el.doctor.lastName}</TableCell>
+    <section className="border-1 rounded-[6px] border-turquoise-600 w-1/2">
+      <div className="p-4 pb-2">
+        <h2 className="text-2xl font-semibold">Liste des patients par horaires</h2>
+      </div>
+      <div className="border-b border-gray-300">
+        <Table className="table-auto w-full">
+          <TableHeader>
+            <TableRow className="border-gray-300">
+              <TableHead className="w-[100px] font-bold">Heure</TableHead>
+              <TableHead className="text-right font-bold">Nom</TableHead>
+              <TableHead className="text-right font-bold">Prénom</TableHead>
+              <TableHead className="text-right font-bold">Médecin</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHeader>
+        </Table>
+      </div>
+      <div className="overflow-y-scroll max-h-[200px]">
+        <Table className="table-auto w-full">
+          <TableBody>
+            {req.data?.getConsultationByDay.map((el) => (
+              <TableRow className="border-none" key={el.id}>
+                <TableCell className="w-[100px]">{dateToTime(new Date(el.date_start))}</TableCell>
+                <TableCell className="text-right">{el.patient.firstName}</TableCell>
+                <TableCell className="text-right">{el.patient.lastName}</TableCell>
+                <TableCell className="text-right font-bold">Dr. {el.doctor.lastName}</TableCell>
+              </TableRow>
+            ))}
+            {/* TODO: à supprimer après le test */}
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+            <TableRow className="border-none">
+              <TableCell className="w-[100px]">10:00</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right">test</TableCell>
+              <TableCell className="text-right font-bold">Dr. test</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </section>
   );
 }
