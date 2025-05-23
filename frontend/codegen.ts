9fc29637497861ee8db1bebf8058770154589d8e
rodolphe <rodolphe.tournier@gmail.com>
2025-05-23 10:38:58 +0200

    fix: codegen update

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -3,7 +3,7 @@ import 'vite';
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: process.env.VITE_APOLLO_SERVER || 'http://localhost:4000/graphql',
+  schema: process.env.VITE_APOLLO_SERVER || 'http://localhost:4000',
   documents: ['src/schemas/**/*.ts'],
   generates: {
     './src/gql/graphql-types.ts': {
