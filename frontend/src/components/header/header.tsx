c5d9abaedec2280b6a0b56c6bfa91021ab5ecd08
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-23 09:05:16 +0200

    Ajout de la redirection sur le logo

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -1,10 +1,13 @@
 import logocp from './logocp.svg';
+import { Link } from 'react-router';
 
 export default function Header() {
   return (
     <header className="w-full bg-white shadow px-6 py-4 flex items-center justify-between">
       {/* Logo + Titre */}
-      <img src={logocp} alt="logo" className="h-auto w-auto" />
+      <Link to="/" className="flex items-center">
+        <img src={logocp} alt="logo" className="h-auto w-auto" />
+      </Link>
     </header>
   );
 }
