50a04ccbc452a3178b2b93c446edee6967fbbeef
Florian Sebal <fsebal91@gmail.com>
2025-05-23 12:36:01 +0200

    Prfix: refacto Adduserform

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,12 +1,10 @@
 import AddUserForm from '@/components/user/AddUserForm';
-import SelectServiceForm from '@/components/service/SelectServiceForm';
 
 const AdminPage = () => {
   return (
     <div>
       <h1>Bienvenue Admin</h1>
       <AddUserForm />
-      <SelectServiceForm />
     </div>
   );
 };
