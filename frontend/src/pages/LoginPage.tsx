b37869e7e0b00247980f8d2102d69ebe219bf0a8
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-23 15:35:56 +0200

    Intégration du nouveau composant login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
-import LoginRoot from '../components/LoginRoot/LoginRoot';
+// import LoginRoot from '../components/LoginRoot/LoginRoot';
 import style from './LoginPage.module.css';
+import Login from '../components/LoginRoot/Login';
 
 function LoginPage() {
   return (
     <div className={`LoginPage ${style.Page}`}>
       <main className={style.Main}>
-        <LoginRoot />
+        {/* <LoginRoot /> */}
+        <Login />
       </main>
     </div>
   );
