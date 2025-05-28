e210b315bae8dc7c0e64914ccb0b71b963dd9ade
Maximilien <maximilien.philippe@protonmail.com>
2025-05-28 17:19:03 +0200

    feat: Add CreatePatient component

---
+++ frontend/src/components/patients/CreatePatient.tsx
@@ -0,0 +1,68 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { InputLabel } from '../ui/input';
+import { Button } from '../ui/button';
+import { SelectLabel } from '../ui/select';
+
+enum GenderInput {
+  male = 'M',
+  female = 'F',
+}
+
+type PatientInput = {
+  ssn: string;
+  firstName: string;
+  lastName: string;
+  birthDate: Date;
+  gender: GenderInput;
+  email: string;
+  codePostal: string;
+  ville: string;
+};
+
+const CreatePatient = () => {
+  const { register, handleSubmit } = useForm<PatientInput>();
+  const submit: SubmitHandler<PatientInput> = (_data) => {};
+  return (
+    <section>
+      <h2 className="font-bold text-3xl">Ajouter un patient</h2>
+      <form onSubmit={handleSubmit(submit)}>
+        <InputLabel
+          id="ssn"
+          label="N° de sécurité sociale"
+          {...register('ssn', { required: true })}
+        />
+        <InputLabel id="nom" label="Nom" {...register('lastName', { required: true })} />
+        <InputLabel id="prenom" label="Prenom" {...register('firstName', { required: true })} />
+        <InputLabel
+          id="date"
+          type="date"
+          label="Date de naissance"
+          {...register('birthDate', { required: true })}
+        />
+        <SelectLabel id="gender" label="Genre" {...register('gender', { required: true })}>
+          <option id="M" value="M">
+            M
+          </option>
+          <option id="F" value="F">
+            F
+          </option>
+        </SelectLabel>
+        <InputLabel
+          type="email"
+          id="email"
+          label="Email"
+          {...register('email', { required: true })}
+        />
+        <InputLabel
+          id="codepostal"
+          label="Code postal"
+          {...register('codePostal', { required: true })}
+        />
+        <InputLabel id="ville" label="Ville" {...register('ville', { required: true })} />
+        <Button type="submit">Enregistrer le nouveau patient</Button>
+      </form>
+    </section>
+  );
+};
+
+export default CreatePatient;
