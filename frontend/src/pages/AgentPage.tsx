dc434a7095f9cb9cc6b7088305fb6a846e69d5ab
Yiroma <yiromaric@gmail.com>
2025-05-28 16:58:00 +0200

    refacto: déplacement du component ConsultationAgentDisplay

--- frontend/src/pages/AgentPage.tsx
+++ frontend/src/pages/AgentPage.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react';
-
-import { useGetConsultationBySsnForAgentLazyQuery } from '@/gql/graphql-types';
-import { formatSSN, unformatSSN } from '@/utils/ssnUtils';
+// components
+import ConsultationAgentDisplay from '@/components/agent/ConsultationAgentDisplay';
 import SsnSearchBar from '@/components/agent/SsnSearchBar';
+// utils
+import { formatSSN, unformatSSN } from '@/utils/ssnUtils';
+// types
+import { useGetConsultationBySsnForAgentLazyQuery } from '@/gql/graphql-types';
 
 const AgentPage = () => {
   const [ssn, setSsn] = useState('');
+
   const [getConsultationBySsnForAgent, { data, loading, error }] =
     useGetConsultationBySsnForAgentLazyQuery({
       onError: (error) => {
@@ -25,13 +29,9 @@ const AgentPage = () => {
       const unformattedSSN = unformatSSN(ssn);
       getConsultationBySsnForAgent({
         variables: { ssn: unformattedSSN },
-        errorPolicy: 'all',
       });
     }
   };
-
-  // Récupération des données de la première consultation trouvée
-  const consultation = data?.getConsultationBySsnForAgent?.[0];
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -39,29 +39,7 @@ const AgentPage = () => {
   return (
     <section className="m-4 h-[calc(100vh-10rem)] flex flex-col">
       <SsnSearchBar handleSubmit={handleSubmit} handleSSNChange={handleSSNChange} ssn={ssn} />
-      <section className="h-2/3 p-4 flex flex-col justify-around items-center gap-2 border border-turquoise-500 rounded-md">
-        {data && data.getConsultationBySsnForAgent.length === 0 ? (
-          <>
-            <p className="text-xl font-bold text-orange-600">Aucune consultation trouvée</p>
-          </>
-        ) : consultation ? (
-          <>
-            <p className="text-xl font-bold">Dr. {consultation.doctor.lastname}</p>
-            <p className="text-lg">{consultation.doctor.service.name}</p>
-            <p className="text-lg font-bold">
-              {consultation.date_start &&
-                new Date(consultation.date_start).toLocaleTimeString('fr-FR', {
-                  hour: '2-digit',
-                  minute: '2-digit',
-                })}
-            </p>
-          </>
-        ) : (
-          <>
-            <p className="text-lg">Entrez un numéro de sécurité sociale ci-dessus</p>
-          </>
-        )}
-      </section>
+      {data && <ConsultationAgentDisplay data={data} />}
     </section>
   );
 };
