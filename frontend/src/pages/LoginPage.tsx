09019dc5f8385cdacf95cda9d8b6bf526886f72b
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-28 12:40:01 +0200

    Retrait du module css

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,9 @@
 import LoginRoot from '../components/LoginRoot/LoginRoot';
-import style from './LoginPage.module.css';
 
 function LoginPage() {
   return (
-    <div className={`LoginPage ${style.Page}`}>
-      <main className={style.Main}>
+    <div className="flex justify-center items-center flex-col w-full p-0 m-0 border-0">
+      <main className="flex justify-center items-center flex-col w-full p-0 m-0 border-0">
         <LoginRoot />
       </main>
     </div>
