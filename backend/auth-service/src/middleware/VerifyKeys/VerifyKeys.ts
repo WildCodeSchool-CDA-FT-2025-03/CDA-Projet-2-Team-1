9072665eafa0ba5b8a12343a108a8c49f411a9e7
Ryan DECIAN <ryan.decian.pro@gmail.com>
2025-05-28 16:46:52 +0200

    Retrait des log dans VerifyKey

--- backend/auth-service/src/middleware/VerifyKeys/VerifyKeys.ts
+++ backend/auth-service/src/middleware/VerifyKeys/VerifyKeys.ts
@@ -12,14 +12,6 @@ function VerifyKeys(requiredKeys: string[]) {
 
     if (missingKeys.length > 0) {
       res.status(400).json({ reponse: 'La syntaxe de la requête est erronée.' });
-      console.error({
-        identity: 'VerifyKeys.ts',
-        type: 'middleware',
-        chemin: '/server/src/middleware/VerifyKeys/VerifyKeys.ts',
-        "❌ Nature de l'erreur": 'Clés manquantes dans la requête',
-        missingKeys,
-        body: req.body,
-      });
       return;
     }
 
