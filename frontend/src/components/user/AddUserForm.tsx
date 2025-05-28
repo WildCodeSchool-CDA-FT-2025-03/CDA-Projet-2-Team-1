a95e6246ade651912b0cecbe599e0c5a07307693
Florian Sebal <fsebal91@gmail.com>
2025-05-28 17:34:29 +0200

    feat: formulaire ajout utilisateur mise en forme

--- frontend/src/components/user/AddUserForm.tsx
+++ frontend/src/components/user/AddUserForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Rolelist from '../role/RoleList';
 import Servicelist from '../service/ServiceList';
+import { InputLabel } from '../ui/input';
 
 function AddUserForm() {
   const [selectedRole, setSelectedRole] = useState('');
@@ -27,38 +28,39 @@ function AddUserForm() {
     <form onSubmit={addUser}>
       <Rolelist value={selectedRole} handleChange={setSelectedRole} />
       <Servicelist value={selectedService} handleChange={setSelectedService} />
-      <label>
-        Nom :
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Prénom :
-        <input
-          type="text"
-          name="firstname"
-          value={firstname}
-          onChange={(e) => setFirstname(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Mél :
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          pattern="[^@\s]+@hopital\.gouv\.fr$"
-          title="email en @hopital.gouv.fr seulement"
-        />
-      </label>
+
+      <InputLabel
+        id="name"
+        label="Nom"
+        type="text"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+
+      <InputLabel
+        id="Prenom"
+        label="Prenom"
+        type="text"
+        name="firstname"
+        value={firstname}
+        onChange={(e) => setFirstname(e.target.value)}
+        required
+      />
+
+      <InputLabel
+        id="email"
+        label="email"
+        type="text"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        pattern="[^@\s]+@hopital\.gouv\.fr$"
+        title="email en @hopital.gouv.fr seulement"
+      />
+
       <button type="submit">Ajouter</button>
     </form>
   );
