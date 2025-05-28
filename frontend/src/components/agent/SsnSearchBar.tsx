04e010abe958c214e361e63d8beec8453eee3a1a
Yiroma <yiromaric@gmail.com>
2025-05-28 16:24:17 +0200

    refacto: déplacement du component SsnSearchBar

---
+++ frontend/src/components/agent/SsnSearchBar.tsx
@@ -0,0 +1,40 @@
+import { Search } from 'lucide-react';
+import React from 'react';
+
+function SsnSearchBar({
+  handleSubmit,
+  handleSSNChange,
+  ssn,
+}: {
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleSSNChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  ssn: string;
+}) {
+  return (
+    <>
+      <form className="h-1/3 flex flex-col justify-center gap-2 my-4" onSubmit={handleSubmit}>
+        <label htmlFor="search">Entrez le numéro de sécurité sociale</label>
+        <div className="flex flex-row gap-2">
+          <input
+            className="w-full bg-white border border-turquoise-500 rounded-md p-2"
+            type="text"
+            id="search"
+            name="ssn"
+            onChange={handleSSNChange}
+            value={ssn}
+            placeholder="1 23 45 67 890 123 45"
+            maxLength={21} // 15 chiffres + 6 espaces pour le format fr
+          />
+          <button
+            className="p-2 flex items-center justify-center rounded-md bg-turquoise-500 hover:bg-turquoise-600"
+            type="submit"
+          >
+            <Search className="w-8 h-8 text-white" />
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default SsnSearchBar;
--- frontend/src/pages/AgentPage.tsx
+++ frontend/src/pages/AgentPage.tsx
@@ -1,8 +1,8 @@
-import { Search } from 'lucide-react';
 import { useState } from 'react';
 
 import { useGetConsultationBySsnForAgentLazyQuery } from '@/gql/graphql-types';
 import { formatSSN, unformatSSN } from '@/utils/ssnUtils';
+import SsnSearchBar from '@/components/agent/SsnSearchBar';
 
 const AgentPage = () => {
   const [ssn, setSsn] = useState('');
@@ -38,27 +38,7 @@ const AgentPage = () => {
 
   return (
     <section className="m-4 h-[calc(100vh-10rem)] flex flex-col">
-      <form className="h-1/3 flex flex-col justify-center gap-2 my-4" onSubmit={handleSubmit}>
-        <label htmlFor="search">Entrez le numéro de sécurité sociale</label>
-        <div className="flex flex-row gap-2">
-          <input
-            className="w-full bg-white border border-turquoise-500 rounded-md p-2"
-            type="text"
-            id="search"
-            name="ssn"
-            onChange={handleSSNChange}
-            value={ssn}
-            placeholder="1 23 45 67 890 123 45"
-            maxLength={21} // 15 chiffres + 6 espaces pour le format fr
-          />
-          <button
-            className="p-2 flex items-center justify-center rounded-md bg-turquoise-500 hover:bg-turquoise-600"
-            type="submit"
-          >
-            <Search className="w-8 h-8 text-white" />
-          </button>
-        </div>
-      </form>
+      <SsnSearchBar handleSubmit={handleSubmit} handleSSNChange={handleSSNChange} ssn={ssn} />
       <section className="h-2/3 p-4 flex flex-col justify-around items-center gap-2 border border-turquoise-500 rounded-md">
         {data && data.getConsultationBySsnForAgent.length === 0 ? (
           <>
