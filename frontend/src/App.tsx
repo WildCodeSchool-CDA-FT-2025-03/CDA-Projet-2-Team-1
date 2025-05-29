6aa31f4f821ae48f422599e0e941df3d32062691
Maximilien <maximilien.philippe@protonmail.com>
2025-05-29 17:33:18 +0200

    feat: Add ModalNavProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router';
 import Header from './components/header/header';
+import { ModalNavProvider } from './context/ModalNav';
 
 function App() {
   return (
     <>
       <Header />
       <div className="app">
-        <Outlet />
+        <ModalNavProvider>
+          <Outlet />
+        </ModalNavProvider>
       </div>
     </>
   );
