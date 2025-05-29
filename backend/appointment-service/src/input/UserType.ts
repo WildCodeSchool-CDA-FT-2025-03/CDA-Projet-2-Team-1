5d59e4a61c54d8ea77f4825c3053814c8de46378
Florian Sebal <fsebal91@gmail.com>
2025-05-29 16:22:48 +0200

    feat: typage mutation

---
+++ backend/appointment-service/src/input/UserType.ts
@@ -0,0 +1,5 @@
+export type Users = {
+  name: string;
+  firstname: string;
+  email: string;
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        turquoise: {
-          500: '#0395d3',
-          600: '#027fb5',
-          DEFAULT: '#0395d3',
-        },
-      },
-    },
-  },
   plugins: [
     // Décommentez les plugins que vous utilisez réellement
     // require('@tailwindcss/forms'),
