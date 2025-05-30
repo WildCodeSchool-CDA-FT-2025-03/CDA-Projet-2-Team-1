4fede9e0ef8edefebbf9e8bc15fa105f5f2906ca
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 13:02:35 +0200

    feat: patient use hook codegen + codegen

--- frontend/src/components/patients/CreatePatient.tsx
+++ frontend/src/components/patients/CreatePatient.tsx
@@ -1,73 +1,70 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { InputLabel } from '../ui/input';
 import { Button } from '../ui/button';
-import { SelectLabel } from '../ui/select';
-
-enum GenderInput {
-  none = 'None',
-  male = 'M',
-  female = 'F',
-}
-
-type PatientInput = {
-  ssn: string;
-  firstName: string;
-  lastName: string;
-  birthDate: Date;
-  gender: GenderInput;
-  email: string;
-  codePostal: string;
-  ville: string;
-};
+import { SelectStyle } from '../ui/select';
+import { AddNewPatientMutationVariables, useAddNewPatientMutation } from '@/gql/graphql-types';
 
 const CreatePatient = () => {
-  const { register, handleSubmit } = useForm<PatientInput>();
-  const submit: SubmitHandler<PatientInput> = (_data) => {};
+  const { register, handleSubmit } = useForm<AddNewPatientMutationVariables>();
+  const [addPatient] = useAddNewPatientMutation();
+
+  const onSubmit: SubmitHandler<AddNewPatientMutationVariables> = (
+    input: AddNewPatientMutationVariables
+  ) => {
+    addPatient({ variables: input });
+  };
+
   return (
     <section>
       <h2 className="font-bold text-3xl mb-4">Ajouter un patient</h2>
-      <form onSubmit={handleSubmit(submit)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <InputLabel
           id="ssn"
           label="N° de sécurité sociale"
-          {...register('ssn', { required: true })}
+          {...register('patient.ssn.number', { required: true })}
         />
-        <InputLabel id="nom" label="Nom" {...register('lastName', { required: true })} />
-        <InputLabel id="prenom" label="Prenom" {...register('firstName', { required: true })} />
+        <InputLabel id="nom" label="Nom" {...register('patient.lastname', { required: true })} />
+        <InputLabel
+          id="prenom"
+          label="Prenom"
+          {...register('patient.firstname', { required: true })}
+        />
         <InputLabel
           id="date"
           type="date"
           label="Date de naissance"
-          {...register('birthDate', { required: true })}
+          {...register('patient.birthdate', { required: true })}
         />
-        <SelectLabel
-          id="gender"
-          label="Genre"
-          className="w-full"
-          {...register('gender', { required: true })}
-        >
-          <option id="None" value="None">
-            --
-          </option>
-          <option id="H" value="H">
-            Homme
-          </option>
-          <option id="F" value="F">
-            Femme
-          </option>
-        </SelectLabel>
+        <div className="mb-4">
+          <label htmlFor="genre">Genre</label>
+          <select className={SelectStyle} id="genre" {...register('patient.gender')}>
+            <option id="None" value="None">
+              --
+            </option>
+            <option id="H" value="M">
+              Homme
+            </option>
+            <option id="F" value="F">
+              Femme
+            </option>
+          </select>
+        </div>
         <InputLabel
           type="email"
           id="email"
           label="Email"
-          {...register('email', { required: true })}
+          {...register('patient.email', { required: true })}
         />
         <InputLabel
           id="codepostal"
           label="Code postal"
-          {...register('codePostal', { required: true })}
+          {...register('patient.city.zip_code', { required: true })}
         />
-        <InputLabel id="ville" label="Ville" {...register('ville', { required: true })} />
+        <InputLabel
+          id="city"
+          label="Ville"
+          {...register('patient.city.name', { required: true })}
+        />
         <Button type="submit">Enregistrer le nouveau patient</Button>
       </form>
     </section>
