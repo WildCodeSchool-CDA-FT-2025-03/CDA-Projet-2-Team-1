c374705149182a787c315844f31c1c8cdaa1b744
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 11:37:02 +0200

    feat: Add SSN validation to SsnInput

--- backend/appointment-service/src/entities/ssn.entity.ts
+++ backend/appointment-service/src/entities/ssn.entity.ts
@@ -1,11 +1,12 @@
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Field, InputType, ObjectType } from 'type-graphql';
-
+import { Matches } from 'class-validator';
 import PatientEntity from './patient.entity';
 
 @InputType()
 export class SsnInput {
   @Field()
+  @Matches(/[0-9]{15}/)
   number: string;
 }
 
