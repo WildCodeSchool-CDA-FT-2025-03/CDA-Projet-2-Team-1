b4b5d551439c99a0067274b73702001c2ba0ed36
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 15:52:38 +0200

    feat: Simplify Button component and remove Select component

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -2,20 +2,18 @@ import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
-const Button = React.forwardRef<HTMLInputElement, React.ComponentProps<'button'>>(
-  ({ className, type, ...props }) => {
-    return (
-      <button
-        type={type}
-        className={cn(
-          'w-full px-4 py-2 bg-main text-white rounded cursor-pointer text-base hover:opacity-90 focus:outline-2 focus:outline-blue-500 focus:outline-offset-2',
-          className
-        )}
-        {...props}
-      />
-    );
-  }
-);
+const Button = ({ className, type, ...props }: React.ComponentProps<'button'>) => {
+  return (
+    <button
+      type={type}
+      className={cn(
+        'w-full px-4 py-2 bg-main text-white rounded cursor-pointer text-base hover:opacity-90 focus:outline-2 focus:outline-blue-500 focus:outline-offset-2',
+        className
+      )}
+      {...props}
+    />
+  );
+};
 Button.displayName = 'Button';
 
 export { Button };
