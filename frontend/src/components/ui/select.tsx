d184b4573a6090466a78bfc84d7a2d84137c15bf
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 13:00:51 +0200

    feat: export style select

--- frontend/src/components/ui/select.tsx
+++ frontend/src/components/ui/select.tsx
@@ -4,6 +4,9 @@ import { cn } from '@/lib/utils';
 
 type SelectProps = React.ComponentProps<'select'> & { children: React.ReactNode };
 
+export const SelectStyle =
+  'flex h-10 w-full rounded-md border border-main bg-background px-3 py-2 text-base ring-offset-background file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-main disabled:cursor-not-allowed disabled:opacity-50 md:text-sm';
+
 const Select = React.forwardRef<HTMLInputElement, SelectProps>(
   ({ className, children, ...props }) => {
     return (
