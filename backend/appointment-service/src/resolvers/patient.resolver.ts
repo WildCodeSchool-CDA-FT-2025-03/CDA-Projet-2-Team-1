a73d80cd641f42f807b46e3c9bec0c444fd942ad
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 09:49:58 +0200

    feat: patient add input

--- backend/appointment-service/src/resolvers/patient.resolver.ts
+++ backend/appointment-service/src/resolvers/patient.resolver.ts
@@ -1,6 +1,32 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
 
 import PatientEntity from '../entities/patient.entity';
+import CityEntity, { CityInput } from '../entities/city.entity';
+import SsnEntity, { SsnInput } from '../entities/ssn.entity';
+
+@InputType()
+class PatientInput {
+  @Field()
+  firstname: string;
+
+  @Field()
+  lastname: string;
+
+  @Field()
+  birthdate: string;
+
+  @Field()
+  gender: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  city: CityInput;
+
+  @Field()
+  ssn: SsnInput;
+}
 
 @Resolver(PatientEntity)
 class PatientResolver {
@@ -15,6 +41,33 @@ class PatientResolver {
       where: { id },
       relations: ['ssn', 'city'],
     });
+  }
+
+  @Mutation(() => String)
+  async addNewPatient(@Arg('patient') patientInput: PatientInput) {
+    let city = await CityEntity.findOneBy({ name: 'Belloy' });
+    if (city === null) {
+      city = new CityEntity();
+      city.name = patientInput.city.name;
+      city.zip_code = patientInput.city.zip_code;
+    }
+    let ssn = await SsnEntity.findOneBy({ number: '012345678901234' });
+    if (ssn === null) {
+      ssn = new SsnEntity();
+      ssn.number = patientInput.ssn.number;
+    }
+
+    const patient = new PatientEntity();
+    patient.firstname = patientInput.firstname;
+    patient.lastname = patientInput.lastname;
+    patient.gender = patientInput.gender;
+    patient.city = city;
+    patient.email = patientInput.email;
+    patient.birthdate = new Date(patientInput.birthdate);
+    patient.ssn = ssn;
+
+    const newPatient = await patient.save();
+    return newPatient.id;
   }
 }
 
