5633336833ac66d69137993a1d25e5aeefdcb701
Maximilien <maximilien.philippe@protonmail.com>
2025-05-30 09:47:53 +0200

    feat: add city input

--- backend/appointment-service/src/entities/city.entity.ts
+++ backend/appointment-service/src/entities/city.entity.ts
@@ -1,7 +1,16 @@
-import { Field, ObjectType } from 'type-graphql';
+import { Field, InputType, ObjectType } from 'type-graphql';
 import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 import PatientEntity from './patient.entity';
+
+@InputType()
+export class CityInput {
+  @Field()
+  name: string;
+
+  @Field()
+  zip_code: string;
+}
 
 @ObjectType()
 @Entity('city')
@@ -11,11 +20,11 @@ class CityEntity extends BaseEntity {
   id: string;
 
   @Field()
-  @Column({ type: 'varchar', length: 128, nullable: false })
+  @Column({ type: 'varchar', length: 128, nullable: false, unique: true })
   name: string;
 
   @Field({ nullable: false })
-  @Column({ type: 'varchar', length: 16, nullable: false })
+  @Column({ type: 'varchar', length: 16, nullable: false, unique: true })
   zip_code: string;
 
   @Field(() => [PatientEntity])
