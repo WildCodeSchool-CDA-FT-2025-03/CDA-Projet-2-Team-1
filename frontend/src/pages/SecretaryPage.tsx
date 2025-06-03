5af704d2165afb618b88c8b177303a1e421ea622
Maximilien <maximilien.philippe@protonmail.com>
2025-06-03 17:38:56 +0200

    feat: consultation list fix date to 00:00:00 for cache

--- frontend/src/pages/SecretaryPage.tsx
+++ frontend/src/pages/SecretaryPage.tsx
@@ -1,10 +1,11 @@
 import ConsultationList from '@/components/consultation/ConsultationList';
 import { PatientDialog } from '@/components/patients/PatientDialog';
+import { dateToDay } from '@/utiles/date.utile';
 
 const SecretaryPage = () => {
   return (
     <>
-      <ConsultationList date={new Date()} />
+      <ConsultationList date={dateToDay(new Date())} />
       <PatientDialog serverUrl={import.meta.env.VITE_APOLLO_SERVER} />
     </>
   );
