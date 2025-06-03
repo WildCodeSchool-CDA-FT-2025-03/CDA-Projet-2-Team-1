d308f1fb1de51a98088ad84e69f37f91b76a2d1c
Maximilien <maximilien.philippe@protonmail.com>
2025-06-03 11:37:52 +0200

    feat: add component button link

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 
 import { cn } from '@/lib/utils';
+import { Link, LinkProps } from 'react-router';
+
+const ButtonStyle =
+  'w-full px-4 py-2 bg-main text-white rounded cursor-pointer text-base hover:opacity-90 focus:outline-2 focus:outline-blue-500 focus:outline-offset-2';
 
 const Button = ({ className, type, ...props }: React.ComponentProps<'button'>) => {
-  return (
-    <button
-      type={type}
-      className={cn(
-        'w-full px-4 py-2 bg-main text-white rounded cursor-pointer text-base hover:opacity-90 focus:outline-2 focus:outline-blue-500 focus:outline-offset-2',
-        className
-      )}
-      {...props}
-    />
-  );
+  return <button type={type} className={cn(ButtonStyle, className)} {...props} />;
 };
 Button.displayName = 'Button';
 
-export { Button };
+const ButtonLink = ({ className, ...props }: LinkProps) => {
+  return (
+    <Link className={cn(`inline-block text-center ${ButtonStyle}`, className)} {...props}></Link>
+  );
+};
+
+export { Button, ButtonLink };
