939e62f4de99a7f2a9c1c7cd5b0c7ff8ad199e5c
Maximilien <maximilien.philippe@protonmail.com>
2025-06-03 11:41:27 +0200

    feat: delete contexte ModalNav

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from 'react-router';
 import Header from './components/header/header';
-import { ModalNavProvider } from './context/ModalNav';
 
 function App() {
   return (
     <>
       <Header />
       <div className="app">
-        <ModalNavProvider>
-          <Outlet />
-        </ModalNavProvider>
+        <Outlet />
       </div>
     </>
   );
