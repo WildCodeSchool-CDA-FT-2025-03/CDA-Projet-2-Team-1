c7d656d90d8a901b54c735350cd4990540cf8b60
Maximilien <maximilien.philippe@protonmail.com>
2025-06-04 12:03:34 +0200

    fix: Add plus icon to patient creation button

--- frontend/src/pages/SecretaryPage.tsx
+++ frontend/src/pages/SecretaryPage.tsx
@@ -1,12 +1,15 @@
 import ConsultationList from '@/components/consultation/ConsultationList';
 import { PatientDialog } from '@/components/patients/PatientDialog';
 import { ButtonLink } from '@/components/ui/button';
+import { Plus } from 'lucide-react';
 import { Outlet } from 'react-router';
 
 const SecretaryPage = () => {
   return (
     <>
-      <ButtonLink to="patient/creation">+ Creation d&apos;un nouveau patient</ButtonLink>
+      <ButtonLink to="patient/creation">
+        <Plus /> {`Création d'un nouveau patient`}
+      </ButtonLink>
       <ConsultationList date={new Date()} />
       <PatientDialog serverUrl={import.meta.env.VITE_APOLLO_SERVER} />
       <Outlet />
