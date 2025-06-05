49590803705663f8849aa0e5a1b669c7058ecc76
Florian Sebal <fsebal91@gmail.com>
2025-06-05 12:31:57 +0200

    feat: mise en place du router

--- backend/auth-service/src/router/router.ts
+++ backend/auth-service/src/router/router.ts
@@ -2,10 +2,12 @@ import express from 'express';
 
 // Import des controllers
 import loginController from '../controllers/loginController';
+import RegisterController from '../controllers/RegisterController';
 
 const router = express.Router();
 
 /* Liste des routes ! */
 router.use('/login', loginController); // 1 route fonctionnelle
+router.use('/register', RegisterController);
 
 export default router;
