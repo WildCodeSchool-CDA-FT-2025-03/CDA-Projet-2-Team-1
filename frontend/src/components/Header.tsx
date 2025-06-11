f3ae8aaa234326652cd0c53bfec5e5afb04709ae
Yiroma <yiromaric@gmail.com>
2025-06-11 21:18:48 +0200

    fix: import logo uniformisé. add: Ajout du logo responsive

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,17 @@
-import logocp from './logocp.svg';
 import { Link } from 'react-router';
+import LogoCarePlanFull from '/logo-cp-full.svg';
+import LogoCarePlan from '/logo-cp.svg';
 
 export default function Header() {
   return (
     <header className="w-full bg-white shadow px-6 py-4 flex items-center justify-between">
-      {/* Logo + Titre */}
       <Link to="/" className="flex items-center">
-        <img src={logocp} alt="logo" className="h-auto w-auto" />
+        <img src={LogoCarePlan} alt="Logo de l'application Care Plan" className="md:hidden h-12" />
+        <img
+          src={LogoCarePlanFull}
+          alt="Logo de l'application Care Plan"
+          className="hidden md:block h-20"
+        />
       </Link>
     </header>
   );
