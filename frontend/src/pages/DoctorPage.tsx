093f43b1d9aaf05254cc5a736c4efd9bb7c5f576
Yiroma <yiromaric@gmail.com>
2025-06-11 23:12:24 +0200

    fix: renommage du dossier

--- frontend/src/pages/DoctorPage.tsx
+++ frontend/src/pages/DoctorPage.tsx
@@ -1,4 +1,4 @@
-import Rest from '@/components/Rest/Rest';
+import Rest from '@/components/oldRest/Rest';
 
 const doctorId = 'b1aef857-4ca5-4da1-bb55-4eb9a05da185'; // À remplacer par l'ID du médecin connecté
 
