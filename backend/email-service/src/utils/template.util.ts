f5f0ac1ce6912ab852ff7e0f16b4e1b5382fb3bb
Maximilien <maximilien.philippe@protonmail.com>
2025-06-12 21:37:06 +0200

    feat: remove async from getTemplate

--- backend/email-service/src/utils/template.util.ts
+++ backend/email-service/src/utils/template.util.ts
@@ -1,9 +1,9 @@
 import { existsSync, readFileSync } from 'node:fs';
 import path from 'node:path';
 
-const ViewPath = '../view';
+const ViewPath = './src/views/';
 
-async function getTemplate(name: string): Promise<string> {
+function getTemplate(name: string): string {
   const templatePath = path.join(ViewPath, name);
   if (!existsSync(templatePath)) {
     throw new Error(`Template ${name} not found`);
