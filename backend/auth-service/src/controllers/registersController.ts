307bd2cb6d6408561408a4a2984d1ba94101b06a
Florian Sebal <fsebal91@gmail.com>
2025-06-13 10:20:38 +0200

    register controller opérationnel

--- backend/auth-service/src/controllers/registersController.ts
+++ backend/auth-service/src/controllers/registersController.ts
@@ -4,12 +4,11 @@ const registersController = express.Router();
 //import des middlewares
 import verifyToken from '../middleware/verifyToken.middleware';
 import validateRole from '../middleware/VerifyKeys/validateRoleQuery.middleware';
-import validateUser from '../middleware/VerifyKeys/validateFields.middleware';
+import validateUser from '../middleware/VerifyKeys/validateUser.middleware';
 
 /* import des utils */
 import { hashPasswordArgonUtil } from '../utils/Argon.utils';
-//import { createUser } from '../repository/user.repository';
-//import { UserInput } from '../types/userTable.type';
+import { createUser } from '../repository/user.repository';
 
 /* import des types */
 
@@ -20,31 +19,16 @@ registersController.post(
   validateUser,
   async (req: Request, res: Response) => {
     try {
-      /* Logique métier 1 : Récupération token cookie et décodage du contenue */
+      /* Logique métier 1 : hashage du password*/
       const hashPassword: string = await hashPasswordArgonUtil(req.body.password);
       req.body.password = hashPassword;
 
-      /**
-       * firstname
-       * lastname
-       * email
-       * hash
-       * gender
-       * role_id
-       * service_id
-       */
-      //console.log('Controller register', req.body);
-      // appel la fonction createUser et que transmette les infos
-      //const newUser: UserInput | null = await createUser(req.body);
-      //console.log('Controller register result', newUser);
-      /* Logique métier 2 : Décodage du token */
-      res.status(200).json({ message: 'test' });
+      const newUser = await createUser(req.body);
+      res.status(200).json(newUser);
     } catch (error) {
-      //console.log('register error', error);
       res.status(500).json({ message: 'Internal server error' });
     }
   }
 );
-// jwttokenservercareplan=
 
 export default registersController;
