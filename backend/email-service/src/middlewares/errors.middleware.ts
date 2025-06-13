0b972537913084bb26528717cc23456c52583292
Maximilien <maximilien.philippe@protonmail.com>
2025-06-13 14:25:18 +0200

    feat: remove message return in error middleware

--- backend/email-service/src/middlewares/errors.middleware.ts
+++ backend/email-service/src/middlewares/errors.middleware.ts
@@ -5,7 +5,6 @@ import logger from '../services/logger.service';
 export default (err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof HttpError) {
     res.status(err.code).json({
-      error: err.message,
       code: err.code,
     });
     logger.error(`${req.method} ${req.get('host') + req.originalUrl}: ${err.message}`);
