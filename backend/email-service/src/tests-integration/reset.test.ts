15cf856601d518112cdd573708fc71dd6b1660f7
Maximilien <maximilien.philippe@protonmail.com>
2025-06-16 08:56:22 +0200

    feat: add test integration bad origin service

--- backend/email-service/src/tests-integration/reset.test.ts
+++ backend/email-service/src/tests-integration/reset.test.ts
@@ -39,4 +39,32 @@ describe('reset email', () => {
     });
     expect(result.status).toBe(400);
   });
+
+  it('Should return 401 Unauthorized (bad origin)', async () => {
+    const payloadErr = {
+      ...payload,
+      serviceOrigin: 'invalid-service',
+    };
+    const token = sign(payloadErr, process.env.SECRET_KEY_TOKEN_SERVER!, {
+      expiresIn: '1h',
+    });
+    const result = await fetch('http://localhost:9501/reset', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+    expect(result.status).toBe(401);
+  });
+
+  it('Should return 401 Unauthorized', async () => {
+    const result = await fetch('http://localhost:9501/reset', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    expect(result.status).toBe(401);
+  });
 });
