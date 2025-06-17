0b036ce8c3af3b48df1bfa4e74b0ce94cb35386c
Florian Sebal <fsebal91@gmail.com>
2025-06-17 12:22:46 +0200

    dataset actualisé avec le bon mot de passe

--- files/datatest/dataset_authentification.sql
+++ files/datatest/dataset_authentification.sql
@@ -15,16 +15,16 @@ INSERT INTO service (name) VALUES
 
 
 INSERT INTO "user" (firstname, lastname, genre, email, password, is_active, role_id, service_id)
-VALUES ('admin', 'admin', 'M', 'admin@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$Vr2Lt4EkmqA34NGDgRrXEQ$fqX/bCwojHsEkdl7REIYhGnn7WzJWXxJwKDIY64ePLQ', true, 1, 1);
+VALUES ('admin', 'admin', 'M', 'admin@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$9kLwM2LM5p75Np20DrNYEw$PXT8wNdqLDdAyaMns9PabvaVJvy24XJBx7fIdEM/2Rk', true, 1, 1);
 
 
 INSERT INTO "user" (firstname, lastname, genre, email, password, is_active, role_id, service_id)
-VALUES ('medecin', 'medecin', 'F', 'medecin@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$Vr2Lt4EkmqA34NGDgRrXEQ$fqX/bCwojHsEkdl7REIYhGnn7WzJWXxJwKDIY64ePLQ', true, 2, 4);
+VALUES ('medecin', 'medecin', 'F', 'medecin@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$9kLwM2LM5p75Np20DrNYEw$PXT8wNdqLDdAyaMns9PabvaVJvy24XJBx7fIdEM/2Rk', true, 2, 4);
 
 
 INSERT INTO "user" (firstname, lastname, genre, email, password, is_active, role_id, service_id)
-VALUES ('secretaire', 'secretaire', 'M', 'secretaire@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$Vr2Lt4EkmqA34NGDgRrXEQ$fqX/bCwojHsEkdl7REIYhGnn7WzJWXxJwKDIY64ePLQ', true, 3, 2);
+VALUES ('secretaire', 'secretaire', 'M', 'secretaire@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$9kLwM2LM5p75Np20DrNYEw$PXT8wNdqLDdAyaMns9PabvaVJvy24XJBx7fIdEM/2Rk', true, 3, 2);
 
 
 INSERT INTO "user" (firstname, lastname, genre, email, password, is_active, role_id, service_id)
-VALUES ('agent', 'agent', 'F', 'agent@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$Vr2Lt4EkmqA34NGDgRrXEQ$fqX/bCwojHsEkdl7REIYhGnn7WzJWXxJwKDIY64ePLQ', true, 4, 3);
+VALUES ('agent', 'agent', 'F', 'agent@hopital.gouv.fr', '$argon2id$v=19$m=65536,t=3,p=4$9kLwM2LM5p75Np20DrNYEw$PXT8wNdqLDdAyaMns9PabvaVJvy24XJBx7fIdEM/2Rk', true, 4, 3);
