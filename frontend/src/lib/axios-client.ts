027d523f81a5fff62fa53b9bffd674740395ef85
Florian Sebal <fsebal91@gmail.com>
2025-06-17 14:18:07 +0200

    axios client avec baseURL /

--- frontend/src/lib/axios-client.ts
+++ frontend/src/lib/axios-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: `/auth`,
+  baseURL: `/`,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
