b3fd7754afb75e3ec4fa6cfb0ccef13e72748451
rodolphe <rodolphe.tournier@gmail.com>
2025-06-18 18:10:43 +0200

    fix: linter pour jest

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { createDefaultPreset } = require("ts-jest");
+const { createDefaultPreset } = require('ts-jest');
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node", // ou "jsdom" si tu testes des composants React
-  testPathIgnorePatterns: ["/node_modules/", "/tests/"], // Ignore les tests Playwright
+  testEnvironment: 'node', // ou "jsdom" si tu testes des composants React
+  testPathIgnorePatterns: ['/node_modules/', '/tests/'], // Ignore les tests Playwright
   transform: {
     ...tsJestTransformCfg,
   },
