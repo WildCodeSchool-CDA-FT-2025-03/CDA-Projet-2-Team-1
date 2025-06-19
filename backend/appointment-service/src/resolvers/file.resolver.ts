b5fd6843bab18bbfdc8ee403af9dc593ba2deb11
Yiroma <yiromaric@gmail.com>
2025-06-20 01:46:25 +0200

    feature: ajout de la mutation pour l'upload

--- backend/appointment-service/src/resolvers/file.resolver.ts
+++ backend/appointment-service/src/resolvers/file.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Arg } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation } from 'type-graphql';
 import ConsultationEntity from '../entities/consultation.entity';
 import FileEntity from '../entities/file.entity';
 
@@ -13,6 +13,32 @@ class FileResolver {
       throw new Error('Consultation not found');
     }
     return FileEntity.find({ where: { consultation } });
+  }
+
+  @Mutation(() => FileEntity)
+  async uploadFile(
+    @Arg('consultationId', () => String) consultationId: string,
+    @Arg('name', () => String) name: string,
+    @Arg('path', () => String) path: string,
+    @Arg('isConfidential', () => Boolean, { defaultValue: false }) isConfidential: boolean = false
+  ): Promise<FileEntity> {
+    // Récupérer la consultation
+    const consultation = await ConsultationEntity.findOne({ where: { id: consultationId } });
+    if (!consultation) {
+      throw new Error('Consultation not found');
+    }
+
+    // Créer le fichier
+    const fileEntity = FileEntity.create({
+      name,
+      path,
+      is_confidential: isConfidential,
+      is_deleted: false,
+      created_at: new Date(),
+      consultation,
+    });
+
+    return await fileEntity.save();
   }
 }
 
