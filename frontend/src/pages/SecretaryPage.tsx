93d6de61f344b004523387033672d8784ea56cc3
Maximilien <maximilien.philippe@protonmail.com>
2025-06-25 17:37:28 +0200

    fix: add calendar

--- frontend/src/pages/SecretaryPage.tsx
+++ frontend/src/pages/SecretaryPage.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router';
+import { useState } from 'react';
 // Components
 import { ButtonLink } from '@/components/ui/button';
 import ConsultationList from '@/components/consultation/ConsultationList';
@@ -7,6 +8,7 @@ import { PatientDialog } from '@/components/patients/PatientDialog';
 import { dateToDay } from '@/utils/date.utility';
 // Styles
 import { Plus } from 'lucide-react';
+import { Calendar } from '@/components/ui/calendar';
 
 const SecretaryPage = () => {
   const [date, setDate] = useState(new Date());
@@ -34,7 +36,7 @@ const SecretaryPage = () => {
       <div className="flex flex-row gap-6 h-[90%]">
         {/* Section à mettre dans le composant calendrier */}
         <section className="w-1/2 flex flex-col justify-center items-center rounded-md border border-turquoise-600 bg-white">
-          Calendrier ici
+          <Calendar mode="single" selected={date} onSelect={setDate} />
         </section>
         <ConsultationList date={dateToDay(new Date())} />
       </div>
