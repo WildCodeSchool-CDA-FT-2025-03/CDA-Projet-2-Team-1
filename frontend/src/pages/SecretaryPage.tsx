0b282015a5b004565db27f46b0da0a657b9813a9
Maximilien <maximilien.philippe@protonmail.com>
2025-06-25 07:18:19 +0200

    feat; add calendar on secretary page

--- frontend/src/pages/SecretaryPage.tsx
+++ frontend/src/pages/SecretaryPage.tsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { ButtonLink } from '@/components/ui/button';
 import ConsultationList from '@/components/consultation/ConsultationList';
 import { Outlet } from 'react-router';
 import { PatientDialog } from '@/components/patients/PatientDialog';
 import { Plus } from 'lucide-react';
 import { dateToDay } from '@/utils/date.utility';
+import { Calendar } from '@/components/ui/calendar';
 
 const SecretaryPage = () => {
+  const [date, setDate] = useState(dateToDay(new Date()));
   return (
     <>
       <section className="flex gap-2 mb-4" aria-label="Actions secrétariat">
@@ -23,6 +26,18 @@ const SecretaryPage = () => {
       </section>
       <ConsultationList date={dateToDay(new Date())} />
       <PatientDialog />
+      <section className={`bg-white p-4 h-[calc(400px+4rem)] flex flex-col w-1/2`}>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="flex-1 rounded-md [&_.rdp-day_selected]:bg-[#0395d3] [&_.rdp-day_selected]:text-white [&_.rdp-day_selected:hover]:bg-[#0284bc]"
+          initialFocus
+          disabled={{ before: new Date() }}
+          fromDate={new Date()}
+          toDate={new Date(new Date().setFullYear(new Date().getFullYear() + 1))}
+        />
+      </section>
       <Outlet />
     </>
   );
