146d9c43466b665b48adce2f7d2620832acca4fc
Maximilien <maximilien.philippe@protonmail.com>
2025-06-25 16:20:41 +0200

    feat: update style

--- frontend/src/components/ui/calendar.tsx
+++ frontend/src/components/ui/calendar.tsx
@@ -1,24 +1,20 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { DayPicker } from 'react-day-picker';
-// import { buttonVariants } from '@/components/ui/button';
-// import { cn } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-// const defaultClassNames = getDefaultClassNames();
-
-function Calendar({ /*className, classNames,*/ showOutsideDays = true, ...props }: CalendarProps) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      // className={cn('h-full flex flex-col', className)}
+      className={cn('h-full flex flex-col', className)}
       classNames={{
         root: 'w-full h-full flex flex-col',
         months: 'flex-1 flex flex-col',
         month: 'flex-1 flex flex-col space-y-4',
-        caption: 'flex justify-between items-center mb-4 px-4',
-        caption_label: 'text-xl font-semibold text-gray-800',
-        nav: 'flex gap-2',
+        caption: 'w-full flex h-12',
+        caption_label: 'mx-auto text-xl font-semibold text-gray-800 lending-none',
+        nav: 'flex gap-2 items-center ml-auto w-8 h-8',
         nav_button:
           'w-10 h-10 flex items-center justify-center rounded-lg border border-gray-300 hover:bg-gray-100 transition-colors',
         nav_button_previous: '',
@@ -30,19 +26,14 @@ function Calendar({ /*className, classNames,*/ showOutsideDays = true, ...props 
         tbody: 'flex-1',
         row: 'grid grid-cols-7 w-full',
         cell: 'p-1',
-        day: 'text-center aspect-square size-[50px] mtext-sm font-medium rounded-lg border border-transparent hover:bg-blue-50 hover:border-blue-200 transition-all duration-200 cursor-pointer',
-        day_today: 'bg-main border-blue-300 text-blue-800 font-semibold',
-        day_selected:
-          'bg-blue-600 border-blue-600 text-white hover:bg-blue-700 hover:border-blue-700',
-        day_outside: 'text-gray-400 hover:bg-gray-50',
+        day: 'text-center aspect-square size-[50px] mtext-sm font-medium rounded-lg hover:bg-blue-50 hover:border-blue-200 transition-all duration-200 cursor-pointer',
+        day_button: 'w-full h-full',
+        today: 'bg-blue-100 text-black',
+        selected: 'bg-main border-blue-600 text-white hover:bg-main hover:border-blue-700',
         day_disabled:
           'text-gray-300 cursor-not-allowed hover:bg-transparent hover:border-transparent',
         day_hidden: 'invisible',
-        // ...classNames,
-      }}
-      components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        ...classNames,
       }}
       {...props}
     />
--- frontend/src/pages/SecretaryPage.tsx
+++ frontend/src/pages/SecretaryPage.tsx
@@ -4,7 +4,6 @@ import ConsultationList from '@/components/consultation/ConsultationList';
 import { Outlet } from 'react-router';
 import { PatientDialog } from '@/components/patients/PatientDialog';
 import { Plus } from 'lucide-react';
-// import { dateToDay } from '@/utils/date.utility';
 import { Calendar } from '@/components/ui/calendar';
 
 const SecretaryPage = () => {
@@ -26,20 +25,9 @@ const SecretaryPage = () => {
       </section>
       <ConsultationList date={date} />
       <PatientDialog />
-      <aside className="lg:col-span-1">
-        <section className={`bg-white p-4 h-[calc(400px+4rem)] flex flex-col w-1/2`}>
-          <Calendar
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            // className="flex-1 rounded-md [&_.rdp-day_selected]:bg-[#0395d3] [&_.rdp-day_selected]:text-white [&_.rdp-day_selected:hover]:bg-[#0284bc]"
-            // autoFocus={true}
-            disabled={{ before: date }}
-            fromDate={new Date()}
-            toDate={new Date(date.setFullYear(new Date().getFullYear() + 1))}
-          />
-        </section>
-      </aside>
+      <section className={`bg-white p-4 h-[calc(400px+4rem)] flex flex-col w-1/2`}>
+        <Calendar mode="single" selected={date} onSelect={setDate} />
+      </section>
       <Outlet />
     </>
   );
